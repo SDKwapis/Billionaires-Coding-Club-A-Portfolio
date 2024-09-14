2a3af4e9d4f6e58d6a8aa312c686bae092c10bdc
SDKwapis <165666421+SDKwapis@users.noreply.github.com>
2024-09-13 21:20:29 -0400

    added profile pic and about me

--- src/components/pages/Hi.jsx
+++ src/components/pages/Hi.jsx
@@ -1,25 +1,29 @@
 import React from "react";
 import "./Hi.css";
-// import image './path-to-image.jpg'; //
+import profileImage from "../../../assets/profile8bit.png";
 
 export default function Hi() {
   return (
     <div className="hi-page">
-      
       <div className="polaroid-container">
-        {/* Polaroid for the image */}
         <div className="polaroid">
-          {/* <img src={YourImage} alt="Stephan Kwapis" className="polaroid-image" /> */}
+          <img
+            src={profileImage}
+            alt="Stephan Kwapis"
+            className="polaroid-image"
+          />
           <p className="caption">Stephan Kwapis</p>
+          <p className="caption2">(a digital representation)</p>
         </div>
 
         {/* Polaroid for the About Me */}
         <div className="polaroid2 about-me">
           <div className="about-me-content">
-            <h2>About Me</h2>
+            <h2 className="about">About Me</h2>
             <p>
-              Hi, I’m Stephan Kwapis! I’m a web developer and graphic designer with a passion for creating beautiful and functional digital experiences. 
-              I love all things tech and design and am constantly learning and evolving in the fast-paced world of development.
+              Hi, I’m Stephan (pronounced Steh-fahn). If you’ve ever looked at a
+              website and thought, “This needs to be prettier and smarter,” then
+              you and I are on the same page.
             </p>
           </div>
         </div>
